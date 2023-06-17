323a3e926acfd2323e930a234191f8fe58bcb3d3
kishore <kishorekarthikeyan.sekhar@prolifytech.com>
2023-06-17 13:28:04 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World! yoo</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World - Updated code!</h1>");
+});
 
-const PORT = 8080
+const PORT = 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
